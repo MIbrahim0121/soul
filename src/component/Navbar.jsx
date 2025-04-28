88a9a3d1004f8c15156150959d1a6935124e4528
MIbrahim0121 <hfzmibrahim295@gmail.com>
2025-04-28 21:13:30 +0500

    change

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='w-[100vw] md:w-full h-[9vh]  font-semibold text-center pt-10 '>
-    <h1 className='pb-5 tracking-wider text-2xl' > AMAZING SOUL</h1> 
+    <h1 className='pb-5 tracking-wider text-3xl' > AMAZING SOUL</h1> 
       <hr  />
     </div>
   )
